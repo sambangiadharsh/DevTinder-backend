2a7f7b115476322c1cc1566faeab9acb25cee509
sambangiadharsh <sambangiadharsh1729@gmail.com>
2025-03-19 22:45:06 +0530

    mute

--- src/sockets.js
+++ src/sockets.js
@@ -11,7 +11,7 @@ const hashRoomId = (userId, targetUserId) => {
 const initializeSockets = (server) => {
   const io = socketIo(server, {
     cors: {
-      origin: 'http://localhost:5173',
+      origin:["http://localhost:5173", "https://tindev.duckdns.org"],
       methods: ['GET', 'POST'],
     },
   });
